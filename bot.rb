85c84f46c67d72c0c3ea37354f1498fe8d91ecb6
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2023-12-13 11:45:09 +0600

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -7,28 +7,22 @@ puts "▶ A simple project made using Ruby Language by 🄺🄰🅁🄼🄰"
 puts "▶ Join @ProjectCodeX"
 
 Telegram::Bot::Client.run(Variables::TOKEN) do |bot|
-  bot_info = bot.api.getMe
-  bot_username = bot_info['result']['username']
-
   bot.listen do |message|
-    command = message.text.split('@')[0].strip
-
-    case command
-    when "/start", "/start@#{bot_username}"
+    case message.text
+    when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}! I am SonicBot, developed using Ruby")
-    when "/bye", "/bye@#{bot_username}"
+    when '/bye'
       bot.api.send_message(chat_id: message.chat.id, text: "Goodbye, #{message.from.first_name}!")
-    when "/ping", "/ping@#{bot_username}"
+    when '/ping'
       start_time = Time.now
       response = bot.api.send_message(chat_id: message.chat.id, text: 'Pong!')
       end_time = Time.now
       duration = (end_time - start_time).round(2)
+
       bot.api.send_message(chat_id: message.chat.id, text: "Ping: #{duration} seconds")
-    when "/joke", "/joke@#{bot_username}"
+    when '/joke'
       joke = get_single_joke(Variables::JOKE_API_ENDPOINT)
       bot.api.send_message(chat_id: message.chat.id, text: joke)
-    else
-      bot.api.send_message(chat_id: message.chat.id, text: "I don't understand that command.")
     end
   end
 end
