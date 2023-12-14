382fb9c146cc5b02fcbf16e1ba4dbb7ed0810a6a
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2023-12-14 12:07:53 +0600

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -1,6 +1,7 @@
 require 'telegram/bot'
 require_relative 'utils'
 require_relative 'variables'
+require_relative 'instadl_handler'
 
 puts "Bot started at #{Time.now}"
 puts "▶ A simple project made using Ruby Language by 🄺🄰🅁🄼🄰"
@@ -23,6 +24,8 @@ Telegram::Bot::Client.run(Variables::TOKEN) do |bot|
     when '/joke'
       joke = get_single_joke(Variables::JOKE_API_ENDPOINT)
       bot.api.send_message(chat_id: message.chat.id, text: joke)
+    when '/instadl'
+      InstadlHandler.handle_instadl(bot, message)
     end
   end
 end
