147ca4e2bde156518c598a981dab09d72c1a771d
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2023-12-14 12:17:23 +0600

    Update variables.rb

--- variables.rb
+++ variables.rb
@@ -1,6 +1,4 @@
 module Variables
   TOKEN = ''
   JOKE_API_ENDPOINT = 'https://karma-api1.vercel.app/joke'
-  DOWNLOADING_STICKER_ID = "CAACAgIAAxkBAAEDv_xlJWmh2-fKRwvLywJaFeGy9wmBKgACVQADr8ZRGmTn_PAl6RC_MAQ"
-  API_URL = "https://karma-api2.vercel.app/instadl"
 end
